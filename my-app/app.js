a6c4ecab0bff04b85c6970f865ecb423a5d27754
Ahmed Hamed <ahmed@example.com>
2026-03-31 10:19:28 +0200

    trigger pipeline

--- my-app/app.js
+++ my-app/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Welcome to Ahmed's CI/CD App with Jenkins!",
+    message: "Hello to Ahmed's CI/CD App with Jenkins!",
     status: "running",
     version: "1.0.0"
   });
